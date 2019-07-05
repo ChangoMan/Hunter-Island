9647294e10f951ac57cee83fc7c59e6e52c9665f
Hunter Chang <hunter@madwiremedia.com>
2019-07-05 12:51:19 -0600

    adding images

--- src/App.js
+++ src/App.js
@@ -9,19 +9,6 @@ class App extends React.Component {
     email: ""
   };
 
-  changeFirstName = event => {
-    // console.log(event.target.value);
-    this.setState({
-      firstName: event.target.value
-    });
-  };
-
-  changeLastName = event => {
-    this.setState({
-      lastName: event.target.value
-    });
-  };
-
   changeInput = event => {
     this.setState({
       [event.target.name]: event.target.value
@@ -31,33 +18,39 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <form>
-          <FormField
-            label="First Name"
-            name="firstName"
-            type="text"
-            value={this.state.firstName}
-            onChange={this.changeInput}
-          />
-          <FormField
-            label="Last Name"
-            name="lastName"
-            type="text"
-            value={this.state.lastName}
-            onChange={this.changeInput}
-          />
-          <FormField
-            label="Email:"
-            name="email"
-            type="email"
-            value={this.state.email}
-            onChange={this.changeInput}
-          />
-        </form>
-        <hr />
-        <h3>First Name: {this.state.firstName}</h3>
-        <h3>Last Name: {this.state.lastName}</h3>
-        <h3>Email: {this.state.email}</h3>
+        <div className="box">
+          <form>
+            <FormField
+              label="First Name"
+              name="firstName"
+              type="text"
+              value={this.state.firstName}
+              onChange={this.changeInput}
+            />
+            <FormField
+              label="Last Name"
+              name="lastName"
+              type="text"
+              value={this.state.lastName}
+              onChange={this.changeInput}
+            />
+            <FormField
+              label="Email:"
+              name="email"
+              type="email"
+              value={this.state.email}
+              onChange={this.changeInput}
+            />
+          </form>
+        </div>
+        <div className="box">
+          <h3>First Name:</h3>
+          <p>{this.state.firstName}</p>
+          <h3>Last Name:</h3>
+          <p>{this.state.lastName}</p>
+          <h3>Email:</h3>
+          <p>{this.state.email}</p>
+        </div>
       </div>
     );
   }
