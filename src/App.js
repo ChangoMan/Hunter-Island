7f3f3ec445cf65c9485568f704995e77c2e9f7b4
Anson Lichtfuss <anson.lichtfuss@madwiremedia.com>
2019-08-02 08:12:35 -0600

    Saving progress from level up

--- src/App.js
+++ src/App.js
@@ -1,43 +1,91 @@
-import React from "react";
-import "./App.css";
-import FormField from "./components/FormField";
+import React from 'react';
+import './App.css';
+import FormField from './components/FormField';
+import UserProfile from './components/UserProfile';
 
 class App extends React.Component {
   state = {
-    firstName: "",
-    lastName: ""
+    firstName: '',
+    lastName: '',
+    invalidFields: [],
+    formSubmitted: false
   };
+
   onChange = event => {
     this.setState({
       [event.target.name]: event.target.value
     });
   };
+
+  onFormSubmitted = event => {
+    event.preventDefault();
+    if (this.validateFields()) {
+      console.log('✅ fields validated, please submit form');
+      this.setState({
+        formSubmitted: true
+      });
+    } else {
+      console.log('🐙 fields NOT valid');
+    }
+  };
+
+  validateFields = () => {
+    const { firstName, lastName } = this.state;
+    const invalidFields = [];
+
+    if (firstName === '') {
+      invalidFields.push('firstName');
+    }
+
+    if (lastName === '') {
+      invalidFields.push('lastName');
+    }
+
+    this.setState({
+      invalidFields
+    });
+
+    if (invalidFields.length > 0) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
   render() {
     return (
       <div className="container">
-        <div className="box">
-          <FormField
-            label="First Name"
-            name="firstName"
-            value={this.state.firstName}
-            onChange={this.onChange}
-          />
-          <div className="form-field">
-            <label>Last Name</label>
-            <input
-              type="text"
-              name="lastName"
-              value={this.state.lastName}
-              onChange={this.onChange}
-            />
+        {this.state.formSubmitted ? (
+          <UserProfile />
+        ) : (
+          <div>
+            <div className="box">
+              <form onSubmit={this.onFormSubmitted}>
+                <FormField
+                  label="First Name"
+                  name="firstName"
+                  value={this.state.firstName}
+                  onChange={this.onChange}
+                  error={this.state.invalidFields.includes('firstName')}
+                />
+                <FormField
+                  label="Last Name"
+                  name="lastName"
+                  value={this.state.lastName}
+                  onChange={this.onChange}
+                  error={this.state.invalidFields.includes('lastName')}
+                />
+                <input type="submit" value="Submit" />
+              </form>
+            </div>
+            <div className="box">
+              <h3>First Name:</h3>
+              <p>{this.state.firstName}</p>
+              <h3>Last Name:</h3>
+              <p>{this.state.lastName}</p>
+            </div>
           </div>
-        </div>
-        <div className="box">
-          <h3>First Name:</h3>
-          <p>{this.state.firstName}</p>
-          <h3>Last Name:</h3>
-          <p>{this.state.lastName}</p>
-        </div>
+        )}
       </div>
     );
   }
